690bcae89408404181e2af4b686c99a8d089140a
mathemartins <mathegeniuse@gmail.com>
2022-04-22 10:39:16 +0100

    modify settings for secret key on heroku deployment

--- yourstudypath/settings.py
+++ yourstudypath/settings.py
@@ -17,7 +17,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+SECRET_KEY = 'django-insecure-1tn0vanm%#&f=hct$)b!2lvvd!makzu347$xj%fb2zip0qtzwk'  # os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get('DEBUG')) == "1"
